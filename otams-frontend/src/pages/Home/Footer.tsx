c2566e827625e4b37284fc2a5746ad0ee549377b
pinkfloyed <pinkiakter195@gmail.com>
2025-10-27 20:11:20 +0600

    feat : Add Footer component
    - Created Footer.tsx to display site footer
    
    - Displays current year dynamically with copyright
    
    - Applied styles from footer.css for consistent layout and color

---
+++ otams-frontend/src/pages/Home/Footer.tsx
@@ -0,0 +1,9 @@
+import "./footer.css";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} Pinki Akter. All rights reserved.</p>
+    </footer>
+  );
+};
